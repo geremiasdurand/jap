3d432e2eff46af9d3344bfa5e8284a0b093888bf
Geremias Durand <geremiasdurand@hotmail.com>
2017-08-16 10:20:01 -0300

    Creacion de clases

---
+++ Obligatorio/Obligatorio/Factura.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Obligatorio
+{
+    class Factura
+    {
+        public DateTime Fecha;
+        public Cliente Cliente;
+        public int MontoTotal;
+        public List<Producto> ListaProductos; 
+        public Factura()
+        {
+            Cliente = new Cliente();
+            ListaProductos = new List<Producto>();
+        }
+    }
+}
---
+++ Obligatorio/Obligatorio/Producto.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Obligatorio
+{
+    class Producto
+    {
+        public int Id;
+        public string Nombre;
+        public string Marca;
+        public int PrecioPorUnidad;
+    }
+}
